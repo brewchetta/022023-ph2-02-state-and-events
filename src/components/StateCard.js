4673b10036671b930abd42af0d9f9d9562932d66
brewchetta <chett.tiller@gmail.com>
2023-03-14 12:02:27 -0400

    states and states and states

--- src/components/StateCard.js
+++ src/components/StateCard.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 
-function StateCard(props) {
+function StateCard({ state }) {
+  
+  const [likes, setLikes] = useState(0)
+  const [color, setColor] = useState(true)
+
+  console.log('likes:', likes)
+  
+  function handleClick() {
+    setLikes( likes + 1 )
+  }
+
+  function handleColor() {
+    setColor( internalColor => !internalColor )
+  }
+
   return (
     <div className="state-card">
+
+      <h2>{ state }</h2>
+
+      <button onClick={ handleClick }>{ likes } Like</button>
+
+      <button 
+      onClick={ handleColor } 
+      style={{ background: color ? 'lightblue' : 'grey' }}>
+        Change Color
+      </button>
+
     </div>
   )
 }
